22d93a0a0e2f353f2bc734aea0fb20528f9d3baf
viktorp999 <viktorpetrovski99@gmail.com>
2022-11-23 23:26:56 +0100

    Commit.

--- ClassInheritanceAndComposition/Composition/Dog.cs
+++ ClassInheritanceAndComposition/Composition/Dog.cs
@@ -4,6 +4,7 @@ namespace ClassInheritanceAndComposition.Composition
     public class Dog
     {
         Eatting eat = new Eatting();
+
         public void DogDoing()
         {
             eat.Eat();
